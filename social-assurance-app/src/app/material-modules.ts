588c8796dc7d025c4b82029dfdb4d735b34e94e4
Allie Rauner <akrauner@gmail.com>
2019-11-10 17:45:48 -0600

    Login page initial

--- social-assurance-app/src/app/material-modules.ts
+++ social-assurance-app/src/app/material-modules.ts
@@ -2,12 +2,14 @@ import { NgModule } from '@angular/core';
 import { MatCardModule } from '@angular/material/card';
 import {MatFormFieldModule} from '@angular/material/form-field'; 
 import {MatInputModule} from '@angular/material/input'; 
+import {MatButtonModule} from '@angular/material/button'; 
 
 @NgModule({
     exports: [
         MatCardModule,
         MatFormFieldModule,
-        MatInputModule
+        MatInputModule,
+        MatButtonModule
     ]
 })
 export class MaterialModule { }
