9b324111e88433792598b9903bf7b8f2ce9ea20f
shengjlim <sheng-jie.lim@huskers.unl.edu>
2019-11-10 16:30:35 -0600

    Added angular app with angular material

--- social-assurance-app/pages/login.js
+++
@@ -1,9 +0,0 @@
-import Layout from '../components/Layout';
-
-export default function Login() {
-  return (
-    <Layout>
-      <p>This is the login  page</p>
-    </Layout>
-  );
-}
---
+++ social-assurance-app/src/app/material-modules.ts
@@ -0,0 +1,14 @@
+import { NgModule } from '@angular/core';
+import { MatCardModule } from '@angular/material/card';
+
+@NgModule({
+    exports: [
+        MatCardModule,
+    ]
+})
+export class MaterialModule { }
+
+
+/**  Copyright 2019 Google LLC. All Rights Reserved.
+    Use of this source code is governed by an MIT-style license that
+    can be found in the LICENSE file at http://angular.io/license */
