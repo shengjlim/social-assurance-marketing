85beaa1473d73d24e061106180e06b45f8b60fd9
shengjlim <sheng-jie.lim@huskers.unl.edu>
2019-11-11 13:30:38 -0600

    Personal Trust Form complete

--- social-assurance-app/src/app/components/personal-trust-form/personal-trust-form.component.ts
+++ social-assurance-app/src/app/components/personal-trust-form/personal-trust-form.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { FormInfoService } from 'src/app/services/form-info.service';
+import { FormControl, Validators } from '@angular/forms';
+import { PersonalTrust } from 'src/app/models/personal-trust';
 
 @Component({
   selector: 'personal-trust-form',
@@ -7,9 +10,30 @@ import { Component, OnInit } from '@angular/core';
 })
 export class PersonalTrustFormComponent implements OnInit {
 
-  constructor() { }
+  constructor(
+    public formInfoService: FormInfoService,
+  ) { }
 
   ngOnInit() {
   }
 
+  connection = new FormControl('', Validators.required);
+  control = new FormControl('', Validators.required);
+  consistency = new FormControl('', Validators.required);
+  commitment = new FormControl('', Validators.required);
+  coCreation = new FormControl('', Validators.required);
+
+  onSubmit() {
+    const personalTrust = new PersonalTrust(
+      this.connection.value,
+      this.control.value,
+      this.consistency.value,
+      this.commitment.value,
+      this.coCreation.value
+    )
+    this.formInfoService.personalTrust = personalTrust
+    console.log(this.formInfoService.brandTrust);
+    console.log(this.formInfoService.innovationTrust);
+    console.log(this.formInfoService.personalTrust);
+  }
 }
