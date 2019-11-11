58cd71b5c71b9c84fe7fe45b175871f79fe48113
shengjlim <sheng-jie.lim@huskers.unl.edu>
2019-11-11 12:59:19 -0600

    Added Submit stuff for Innovation Trust

--- social-assurance-app/src/app/components/innovation-trust-form/innovation-trust-form.component.ts
+++ social-assurance-app/src/app/components/innovation-trust-form/innovation-trust-form.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { FormInfoService } from 'src/app/services/form-info.service';
+import { FormControl, Validators } from '@angular/forms';
+import { InnovationTrust } from 'src/app/models/innovation-trust';
 
 @Component({
   selector: 'innovation-trust-form',
@@ -7,9 +10,25 @@ import { Component, OnInit } from '@angular/core';
 })
 export class InnovationTrustFormComponent implements OnInit {
 
-  constructor() { }
+  constructor(
+    private formInfoService: FormInfoService,
+  ) { }
 
   ngOnInit() {
   }
 
+  relativeTrust = new FormControl('', Validators.required);
+  userExperience = new FormControl('', Validators.required);
+  promise = new FormControl('', Validators.required);
+  socialProof = new FormControl('', Validators.required);
+
+  addInnovationTrust() {
+    const innovationTrust = new InnovationTrust(
+      this.relativeTrust.value,
+      this.userExperience.value,
+      this.promise.value,
+      this.socialProof.value
+    )
+    this.formInfoService.innovationTrust = innovationTrust;
+  }
 }
