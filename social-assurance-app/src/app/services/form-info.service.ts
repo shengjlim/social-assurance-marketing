13353e86e15923052ca0d1048bcbe6e7b6722228
jackarens <jack.arens@huskers.unl.edu>
2019-12-02 09:18:18 -0600

    updating entries instead of duplicating

--- social-assurance-app/src/app/services/form-info.service.ts
+++ social-assurance-app/src/app/services/form-info.service.ts
@@ -12,14 +12,41 @@ export class FormInfoService {
   constructor(private db: AngularFirestore) { }
 
   putPersonalTrustObject(personalTrust){
-    this.db.collection('personal').add(personalTrust);
+    this.db.collection("personal", ref => ref.where("email", "==", personalTrust.email)).get().subscribe(data => {
+      if(data.docs[0].data() != undefined) {
+        this.db.collection('personal').doc(data.docs[0].id).delete().then(function() {
+            console.log("Document successfully deleted!");
+        }).catch(function(error) {
+            console.error("Error removing document: ", error);
+        });
+      }
+      this.db.collection('personal').add(personalTrust);
+    });
   }
 
   putBrandTrustObject(brandTrust){
-    this.db.collection('brand').add(brandTrust);
+    this.db.collection("brand", ref => ref.where("groupId", "==", brandTrust.groupId)).get().subscribe(data => {
+      if(data.docs[0].data() != undefined) {
+        this.db.collection('brand').doc(data.docs[0].id).delete().then(function() {
+            console.log("Document successfully deleted!");
+        }).catch(function(error) {
+            console.error("Error removing document: ", error);
+        });
+      }
+      this.db.collection('brand').add(brandTrust);
+    });
   }
 
   putInnovationTrustObject(innovationTrust){
-    this.db.collection('innovation').add(innovationTrust);
+    this.db.collection("innovation", ref => ref.where("groupId", "==", innovationTrust.groupId)).get().subscribe(data => {
+      if(data.docs[0].data() != undefined) {
+        this.db.collection('innovation').doc(data.docs[0].id).delete().then(function() {
+            console.log("Document successfully deleted!");
+        }).catch(function(error) {
+            console.error("Error removing document: ", error);
+        });
+      }
+      this.db.collection('innovation').add(innovationTrust);
+    });
   }
 }
